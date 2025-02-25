1f5f02d951d46edce202022140607086ee2e6060
Aleena Ali Azeem <99934126+aleenaazeem@users.noreply.github.com>
2025-02-24 23:50:00 -0500

    Create WordEntry.java

---
+++ WordCompletionAVL/src/avlwordcompletion/WordEntry.java
@@ -0,0 +1,11 @@
+package avlwordcompletion;
+
+public class WordEntry {
+    String word;
+    int frequency;
+
+    public WordEntry(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+}
